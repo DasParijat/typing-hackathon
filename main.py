378c79eb6f675bb9dab9aeae5c7b14e5c143379b
DasParijat <143519231+DasParijat@users.noreply.github.com>
2024-11-16 16:42:52 -0500

    Highlight word user is supposed to type
    Also more code formatting stuff

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class TypingTest:
         self.look_ahead: int = look_ahead
 
     def start_test(self):
-        # init variables for test
+        """init variables for test"""
         random_words = random.sample(self.words, 5)
         self.test_words: list[str] = list(map(str.strip, random_words))
         self.amount_correct_chars: int = 0
@@ -38,18 +38,20 @@ class TypingTest:
         self.end_test()
 
     def do_test(self):
+        """Gets user input and current word they're trying to type"""
         for i in range(len(self.test_words)):
             match_word = self.test_words[i]
             words = self.test_words[
-                i : min(len(self.test_words), i + self.look_ahead + 1)
+                i + 1 : min(len(self.test_words), i + self.look_ahead + 1)
             ]
 
-            print(" ".join(words))
+            print(f">>{match_word}<< " + " ".join(words))
             user_word = input()
             self.process_word(match_word, user_word)
             print()
 
     def end_test(self):
+        """Calculates and prints WPM"""
         end_time = datetime.now()
         difference = end_time - self.start_time
 
@@ -63,6 +65,24 @@ class TypingTest:
         print(f"Your adjusted raw wpm is {adjusted_wpm:.2f}")
         print(f"Your raw wpm is {raw_wpm:.2f}")
 
+    def process_word(self, match_word: str, user_word: str):
+        """Determines correctness of input, and records input for replay"""
+        self.word_inputs.append((user_word, datetime.now()))
+
+        match_word = match_word + "#" * (len(user_word) - len(match_word))
+        user_word = user_word + "#" * (len(match_word) - len(user_word))
+        
+        for i in range(len(user_word)):
+            user_char = user_word[i]
+            matching_char = match_word[i]
+        
+            if user_char == matching_char:
+                self.amount_correct_chars += 1
+                print(to_color(matching_char, GREEN), end="")
+            else:
+                print(to_color(matching_char, RED), end="")
+
+            
     def do_replay(self):
         """Replays previous game"""
         these_word_inputs = self.word_inputs.copy()
@@ -85,23 +105,6 @@ class TypingTest:
             print()
             self.process_word(self.test_words[i], user_word)
             print()
-
-    def process_word(self, match_word: str, user_word: str):
-        """Determines correctness of input, and records input for replay"""
-        self.word_inputs.append((user_word, datetime.now()))
-
-        match_word = match_word + "#" * (len(user_word) - len(match_word))
-        user_word = user_word + "#" * (len(match_word) - len(user_word))
-        
-        for i in range(len(user_word)):
-            user_char = user_word[i]
-            matching_char = match_word[i]
-        
-            if user_char == matching_char:
-                self.amount_correct_chars += 1
-                print(to_color(matching_char, GREEN), end="")
-            else:
-                print(to_color(matching_char, RED), end="")
 
 def main():
     sample_size = START_SAMPLE_SIZE
